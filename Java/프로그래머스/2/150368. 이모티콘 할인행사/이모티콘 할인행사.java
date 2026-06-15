8c1d135aa73e2b97c63699ba7b1d37a96078a640
강휘윤 <126214324+Creamcheesepie@users.noreply.github.com>
2026-06-15 16:41:44 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 31.85 ms, Memory: 79 MB -BaekjoonHub

--- Java/프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ Java/프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,48 +1,54 @@
 import java.util.*;
 
 class Solution {
-    int[][] lUsers;
-    int[] lEmoticons;
-    int[] answer = new int[2];
+    int[] discounts = new int[] {10,20,30,40};
+    int maxSubs = 0;
+    int maxSales = 0;
+    
     public int[] solution(int[][] users, int[] emoticons) {
-        lUsers = users;
-        lEmoticons = emoticons;
-        calcPrice(0, new int[emoticons.length]);
-        return answer;
+        getBestCase(0, new int[emoticons.length], users, emoticons);
+        return new int[] {maxSubs, maxSales};
     }
     
-    private void calcUsers(int[] currentDiscounts){
-        int subsSum = 0;
-        int soldTotal = 0;
-        for(int[] user : lUsers){
-            int buyTotal = 0;
-            int buyRate = user[0];
-            int subsPrice = user[1];
-            for(int i = 0; i<lEmoticons.length; i++){
-                if(buyRate <= currentDiscounts[i]){
-                    buyTotal += lEmoticons[i] - ((lEmoticons[i] / 100) * currentDiscounts[i]);
+    private void calcUsers(int[] currentDiscounts,int[][] users, int[] emoticons){
+        int subsCnt = 0;
+        int totalSales = 0;
+       
+        for(int[] user : users){
+            int userDiscountLimit = user[0];
+            int userSubsLimit = user[1];
+            int purchaseAmount = 0;
+            
+            for(int i = 0; i<emoticons.length; i++){
+                if(currentDiscounts[i] >= userDiscountLimit){
+                    purchaseAmount += emoticons[i] * (100 - currentDiscounts[i]) / 100;
                 }
             }
-            if(buyTotal >= subsPrice) subsSum ++;
-            else soldTotal += buyTotal;
+            
+            if(purchaseAmount >= userSubsLimit){
+                subsCnt++;
+            } else {
+                totalSales += purchaseAmount;
+            }
         }
-        if(answer[0] < subsSum){
-            answer[0] = subsSum;
-            answer[1] = soldTotal;
-        } else if(answer[0] == subsSum){
-            answer[1] = Math.max(answer[1],soldTotal);
+        
+        if(subsCnt > maxSubs ||
+            (subsCnt == maxSubs && totalSales > maxSales)
+          ){
+            maxSubs = subsCnt;
+            maxSales = totalSales;
         }
     }
     
-    private void calcPrice(int idx, int[] currentDiscounts){
+    private void getBestCase(int idx, int[] currentDiscounts,int[][] users, int[] emoticons){
         if(idx == currentDiscounts.length){
-            calcUsers(currentDiscounts);
+            calcUsers(currentDiscounts,users,emoticons);
             return;
         }
         
-        for(int rate : new int[] {10,20,30,40}){
+        for(int rate : discounts){
             currentDiscounts[idx] = rate;
-            calcPrice(idx + 1, currentDiscounts);
+            getBestCase(idx + 1, currentDiscounts,users,emoticons);
         }
     }
 }
